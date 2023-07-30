1acf109431c3ef91a653155c97f79e72c90eed6b
Charmaine <charmaine.thq@gmail.com>
2023-07-30 16:48:51 +0800

    idk

--- app/src/main/java/com/example/ad_spotifyclone/HomeActivity.java
+++ app/src/main/java/com/example/ad_spotifyclone/HomeActivity.java
@@ -10,11 +10,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -92,6 +102,72 @@ public class HomeActivity extends AppCompatActivity {
         // This function will be called when the "Submit Log File" button is clicked.
         // Implement the logic to send the log file to your desired server or save it for later analysis.
         saveLogFile();
+
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    String boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW";
+//                    String LINE_FEED = "\r\n";
+//
+//                    URL url = new URL("https://yourserver.com/upload");
+//                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+//                    httpConn.setUseCaches(false);
+//                    httpConn.setDoOutput(true); // indicates POST method
+//                    httpConn.setDoInput(true);
+//                    httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+//
+//                    OutputStream outputStream = httpConn.getOutputStream();
+//                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, Charset.defaultCharset()), true);
+//
+//                    File uploadFile = new File(getFilesDir(), filename);
+//                    String fileName = uploadFile.getName();
+//                    writer.append("--" + boundary).append(LINE_FEED);
+//                    writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+//                    writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+//                    writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+//                    writer.append(LINE_FEED);
+//                    writer.flush();
+//
+//                    FileInputStream inputStream = new FileInputStream(uploadFile);
+//                    byte[] buffer = new byte[4096];
+//                    int bytesRead = -1;
+//                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                        outputStream.write(buffer, 0, bytesRead);
+//                    }
+//                    outputStream.flush();
+//                    inputStream.close();
+//                    writer.append(LINE_FEED).flush();
+//                    writer.append("--" + boundary + "--").append(LINE_FEED);
+//                    writer.close();
+//
+//                    // checks server's status code first
+//                    int status = httpConn.getResponseCode();
+//                    if (status == HttpURLConnection.HTTP_OK) {
+//                        // server response is OK
+//                        InputStream responseStream = new BufferedInputStream(httpConn.getInputStream());
+//                        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+//                        String line = "";
+//                        StringBuilder stringBuilder = new StringBuilder();
+//                        while ((line = responseStreamReader.readLine()) != null) {
+//                            stringBuilder.append(line).append("\n");
+//                        }
+//                        responseStreamReader.close();
+//
+//                        String response = stringBuilder.toString();
+//                        // use response for further processing
+//                    } else {
+//                        throw new IOException("Server returned non-OK status: " + status);
+//                    }
+//                    httpConn.disconnect();
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        });
+//        thread.start();
+
+
     }
 
     private void saveLogFile(){
