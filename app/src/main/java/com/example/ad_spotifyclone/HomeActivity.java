8d273022acbdf03c0329d84dd08a010a41aac20f
Charmaine <charmaine.thq@gmail.com>
2023-07-31 16:59:46 +0800

    send log logic

--- app/src/main/java/com/example/ad_spotifyclone/HomeActivity.java
+++ app/src/main/java/com/example/ad_spotifyclone/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.ad_spotifyclone;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -97,78 +99,94 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+    String filename;
     private void submitLogFile() {
         // Code to submit the log file goes here.
         // This function will be called when the "Submit Log File" button is clicked.
         // Implement the logic to send the log file to your desired server or save it for later analysis.
-        saveLogFile();
+        createDummyLogFile();
 
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    String boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW";
-//                    String LINE_FEED = "\r\n";
-//
-//                    URL url = new URL("https://yourserver.com/upload");
-//                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-//                    httpConn.setUseCaches(false);
-//                    httpConn.setDoOutput(true); // indicates POST method
-//                    httpConn.setDoInput(true);
-//                    httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-//
-//                    OutputStream outputStream = httpConn.getOutputStream();
-//                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, Charset.defaultCharset()), true);
-//
-//                    File uploadFile = new File(getFilesDir(), filename);
-//                    String fileName = uploadFile.getName();
-//                    writer.append("--" + boundary).append(LINE_FEED);
-//                    writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"").append(LINE_FEED);
-//                    writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
-//                    writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
-//                    writer.append(LINE_FEED);
-//                    writer.flush();
-//
-//                    FileInputStream inputStream = new FileInputStream(uploadFile);
-//                    byte[] buffer = new byte[4096];
-//                    int bytesRead = -1;
-//                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-//                        outputStream.write(buffer, 0, bytesRead);
-//                    }
-//                    outputStream.flush();
-//                    inputStream.close();
-//                    writer.append(LINE_FEED).flush();
-//                    writer.append("--" + boundary + "--").append(LINE_FEED);
-//                    writer.close();
-//
-//                    // checks server's status code first
-//                    int status = httpConn.getResponseCode();
-//                    if (status == HttpURLConnection.HTTP_OK) {
-//                        // server response is OK
-//                        InputStream responseStream = new BufferedInputStream(httpConn.getInputStream());
-//                        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
-//                        String line = "";
-//                        StringBuilder stringBuilder = new StringBuilder();
-//                        while ((line = responseStreamReader.readLine()) != null) {
-//                            stringBuilder.append(line).append("\n");
-//                        }
-//                        responseStreamReader.close();
-//
-//                        String response = stringBuilder.toString();
-//                        // use response for further processing
-//                    } else {
-//                        throw new IOException("Server returned non-OK status: " + status);
-//                    }
-//                    httpConn.disconnect();
-//                } catch (Exception ex) {
-//                    ex.printStackTrace();
-//                }
-//            }
-//        });
-//        thread.start();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW";
+                    String LINE_FEED = "\r\n";
+
+                    URL url = new URL("http://172.188.27.126:8080/logs/upload");
+                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                    httpConn.setUseCaches(false);
+                    httpConn.setDoOutput(true); // indicates POST method
+                    httpConn.setDoInput(true);
+                    httpConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+                    OutputStream outputStream = httpConn.getOutputStream();
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, Charset.defaultCharset()), true);
+
+                    File uploadFile = new File(getFilesDir(), filename);
+                    String fileName = uploadFile.getName();
+                    writer.append("--" + boundary).append(LINE_FEED);
+                    writer.append("Content-Disposition: form-data; name=\"logFile\"; filename=\"" + fileName + "\"").append(LINE_FEED);
+                    writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_FEED);
+                    writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+                    writer.append(LINE_FEED);
+                    writer.flush();
+
+                    FileInputStream inputStream = new FileInputStream(uploadFile);
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = -1;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                    outputStream.flush();
+                    inputStream.close();
+                    writer.append(LINE_FEED).flush();
+                    writer.append("--" + boundary + "--").append(LINE_FEED);
+                    writer.close();
+
+                    // checks server's status code first
+                    int status = httpConn.getResponseCode();
+                    if (status == HttpURLConnection.HTTP_OK) {
+                        // server response is OK
+                        InputStream responseStream = new BufferedInputStream(httpConn.getInputStream());
+                        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+                        String line = "";
+                        StringBuilder stringBuilder = new StringBuilder();
+                        while ((line = responseStreamReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        responseStreamReader.close();
+
+                        String response = stringBuilder.toString();
+                        // use response for further processing
+                    } else {
+                        throw new IOException("Server returned non-OK status: " + status);
+                    }
+                    httpConn.disconnect();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        thread.start();
 
 
     }
+    // Only for testing sending
+    protected void createDummyLogFile() {
+        filename = "logfile.log";
+        String fileContents = "Test log data...\nMore log data...";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private void saveLogFile(){
         // save the log file to the app storage as a txt file first
